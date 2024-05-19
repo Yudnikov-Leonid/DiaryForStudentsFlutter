fbe0822c38cb69bf33ba19b126e70bb7ee7ee91e
Diryfort <dorhungmi2@gmail.com>
2024-05-19 13:55:01 +0300

    feature/performance actual quarter on init

--- lib/features/performance/data/repository/performance_repository_impl.dart
+++ lib/features/performance/data/repository/performance_repository_impl.dart
@@ -4,6 +4,7 @@ import 'package:edu_diary/features/performance/data/models/final_response.dart';
 import 'package:edu_diary/features/performance/data/models/lesson.dart';
 import 'package:edu_diary/features/performance/domain/entities/lesson.dart';
 import 'package:edu_diary/features/performance/domain/repository/performance_repository.dart';
+import 'package:intl/intl.dart';
 
 class PerformanceRepositoryImpl implements PerformanceRepository {
   final PerformanceDataSource _dataSource;
@@ -13,19 +14,36 @@ class PerformanceRepositoryImpl implements PerformanceRepository {
   PerformanceFinalResponse? _cachedFinalResponse;
   List<(String, String)> _periods = [];
 
+  int _currentQuarter = 4;
+
   @override
   Future<DataState<(List<LessonModel>, int)>> getLessons() async {
     try {
       if (_periods.isEmpty) {
         _periods = await _dataSource.getPeriods();
+        final format = DateFormat('dd.MM.yyyy');
+        final intPeriods = _periods
+            .map((pair) => (
+                  format.parse(pair.$1).millisecondsSinceEpoch ~/ 86400000,
+                  format.parse(pair.$2).millisecondsSinceEpoch ~/ 86400000
+                ))
+            .toList();
+        final today = DateTime.now().millisecondsSinceEpoch ~/ 86400000;
+        for (int i = 0; i < 3; i++) {
+          if (intPeriods[i].$1 > today && today < intPeriods[i + 1].$1) {
+            _currentQuarter = i + 1;
+            break;
+          }
+        }
       }
       _cachedFinalResponse = await _dataSource.getFinalLessons();
       _cachedAverageMap.clear();
       _cachedFinalResponse!.lessons!.forEach((element) {
-        _cachedAverageMap[element.name] = element.data[2 - 1].$1;
+        _cachedAverageMap[element.name] = element.data[_currentQuarter - 1].$1;
       });
-      final list = await _dataSource.getLessons(_periods[2 - 1], _cachedAverageMap);
-      return DataSuccess((list.lessons!, 2));
+      final list =
+          await _dataSource.getLessons(_periods[_currentQuarter - 1], _cachedAverageMap);
+      return DataSuccess((list.lessons!, _currentQuarter));
     } catch (e) {
       return DataFailed(e.toString());
     }
@@ -33,15 +51,20 @@ class PerformanceRepositoryImpl implements PerformanceRepository {
 
   @override
   Future<DataState<List<LessonEntity>>> changeQuarter(int newQuarter) async {
+    _currentQuarter = newQuarter;
     try {
       _cachedAverageMap.clear();
       _cachedFinalResponse!.lessons!.forEach((element) {
         _cachedAverageMap[element.name] = element.data[newQuarter - 1].$1;
       });
-      final list = await _dataSource.getLessons(_periods[newQuarter - 1], _cachedAverageMap);
+      final list = await _dataSource.getLessons(
+          _periods[newQuarter - 1], _cachedAverageMap);
       return DataSuccess(list.lessons!);
     } catch (e) {
       return DataFailed(e.toString());
     }
   }
+
+  @override
+  int currentQuarter() => _currentQuarter;
 }
--- lib/features/performance/domain/repository/performance_repository.dart
+++ lib/features/performance/domain/repository/performance_repository.dart
@@ -4,4 +4,5 @@ import 'package:edu_diary/features/performance/domain/entities/lesson.dart';
 abstract class PerformanceRepository {
   Future<DataState<(List<LessonEntity>, int)>> getLessons();
   Future<DataState<List<LessonEntity>>> changeQuarter(int newQuarter);
+  int currentQuarter();
 }
